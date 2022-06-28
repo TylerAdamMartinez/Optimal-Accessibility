efd0f0687e70afe2741ea475284b1a0f256579a4
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-06-27 21:39:44 -0500

    [App] feat: add template data for OverallAccessibilitySection

--- Application/src/App.js
+++ Application/src/App.js
@@ -4,12 +4,40 @@ import MyPostersSection from './Components/MyPostersSection';
 import OverallAccessibilitySection from './Components/OverallAccessibilitySection';
 
 function App() {
+  let OverallAccessibilityBarGraphData = {
+    labels: ['Text', 'Structure', 'Color'],
+    datasets: [
+      {
+        label: 'Rating Score',
+        backgroundColor: [
+          'rgba(1, 127, 1, 1)',
+          'rgba(100, 6, 101, 1)',
+          'rgba(218, 54, 74, 1)',
+        ],
+        borderColor: 'rgba(51, 51, 51, 1)',
+        borderWidth: 1,
+        data: [65, 59, 80],
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: {
+              min: 0,
+              max: 100,
+            },
+          }
+        },
+      }
+    ]
+  };
+
+
   return (
     <>
       <NavBar />
       <div className="App">
         <MyPostersSection />
-        <OverallAccessibilitySection />
+        <OverallAccessibilitySection chartData={OverallAccessibilityBarGraphData} />
       </div>
     </>
   );
