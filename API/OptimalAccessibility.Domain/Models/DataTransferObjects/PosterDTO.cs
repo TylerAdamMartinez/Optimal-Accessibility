6f5d18a7c4adcb575ce7119ceb4db7d12ab107ef
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-01 13:33:53 -0500

    [DataTransferObjects] feat: add PosterDTO class

---
+++ API/OptimalAccessibility.Domain/Models/DataTransferObjects/PosterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using OptimalAccessibility.Domain.Models.Database;
+
+namespace OptimalAccessibility.Domain.Models.DataTransferObjects
+{
+    public class PosterDTO
+    {
+        [Key]
+        public Guid posterId { get; set; }
+        [Required]
+        public Guid userId { get; set; }
+        [Required]
+        public string PosterName { get; set; } = string.Empty;
+        public AccessibilityScore? AccessibilityScore { get; set; }
+    }
+}
+
