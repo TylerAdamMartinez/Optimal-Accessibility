2470f2dcb86f0a8c0ba15250cc0bac032a799180
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-01 13:36:20 -0500

    [UserDTO] feat: add AccessibilityScore & List of PosterDTOs

--- API/OptimalAccessibility.Domain/Models/DataTransferObjects/UserDTO.cs
+++ API/OptimalAccessibility.Domain/Models/DataTransferObjects/UserDTO.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
+using OptimalAccessibility.Domain.Models.Database;
+
 namespace OptimalAccessibility.Domain.Models.DataTransferObjects
 {
     public class UserDTO
     {
+        [Key]
         public Guid UserId { get; set; }
-        public string EUID { get; set; }
-        public string FristName { get; set; }
-        public string LastName { get; set; }
+        [Required]
+        public string EUID { get; set; } = string.Empty;
+        public string FristName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public List<PosterDTO>? posters { get; set; }
+        public AccessibilityScore? AccessibilityScore { get; set; }
     }
 }
 
