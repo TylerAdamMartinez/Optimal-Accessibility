6c0cb3759fcbac6a030fbc8167c307cbe2b9bca4
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-01 06:53:12 -0500

    [UserController] Update get users http get method

--- API/OptimalAccessibility.API/Controllers/UserController.cs
+++ API/OptimalAccessibility.API/Controllers/UserController.cs
@@ -1,32 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace OptimalAccessibility.API.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+namespace OptimalAccessibility.API.Controllers
 {
-    private static readonly string[] Summaries = new[]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private readonly ILogger<UserController> _logger;
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        public UserController(ILogger<UserController> logger)
         {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
     }
 }
