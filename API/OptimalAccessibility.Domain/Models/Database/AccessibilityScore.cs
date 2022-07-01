3d38f8cc2e9b1b7da4c9d1c7d8a58f2c6e03c9d7
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-01 13:34:38 -0500

    [Database] feat: add AccessibilityScore and Poster

---
+++ API/OptimalAccessibility.Domain/Models/Database/AccessibilityScore.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OptimalAccessibility.Domain.Models.Database
+{
+    public class AccessibilityScore
+    {
+        [Key]
+        public Guid accessibilityScoreId { get; set; }
+        [Required]
+        public Guid userID { get; set; }
+
+        public int TextRating { get; set; } = default;
+        public int StructureRating { get; set; } = default;
+        public int ColorRating { get; set; } = default;
+    }
+}
+
---
+++ API/OptimalAccessibility.Domain/Models/Database/Poster.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace OptimalAccessibility.Domain.Models.Database
+{
+    public class Poster
+    {
+        [Key]
+        public Guid posterId { get; set; }
+        [Required]
+        public Guid userId { get; set; }
+        [Required]
+        public string PosterName { get; set; } = string.Empty;
+        public string PosterImageTitle { get; set; } = string.Empty;
+        public byte[]? PosterImageData { get; set; } = default;
+        public Guid? accessibilityScoreId { get; set; }
+    }
+}
+
