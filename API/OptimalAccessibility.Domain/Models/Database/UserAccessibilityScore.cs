7234f9dfd018bf8652c813d77f4552d4c55d2594
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-03 23:12:07 -0500

    [Database] refactor: split AccesibilityScore table
    AccesibilityScore table split into PosterAccessibilityScore and UserAccessibilityScore

--- API/OptimalAccessibility.Domain/Models/Database/UserAccessibilityScore.cs
+++ API/OptimalAccessibility.Domain/Models/Database/UserAccessibilityScore.cs
@@ -1,17 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OptimalAccessibility.Domain.Models.Database
 {
-    public class AccessibilityScore
+    [Table("UserAccessiblityScore")]
+    public class UserAccessibilityScore
     {
         [Key]
         public Guid accessibilityScoreId { get; set; }
-        [Required]
         public Guid userId { get; set; }
-        public Guid? posterId { get; set; }
 
+        [Column("Text")]
         public int TextRating { get; set; } = default;
+        [Column("Structure")]
         public int StructureRating { get; set; } = default;
+        [Column("Color")]
         public int ColorRating { get; set; } = default;
     }
 }
