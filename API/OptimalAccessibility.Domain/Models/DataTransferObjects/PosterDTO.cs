f77a0ccb81e294890820e100a2a1d5f472bfe6f5
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-03 23:14:50 -0500

    [DataTransferObjects] feat: change AccessibilityScore to DTO version

--- API/OptimalAccessibility.Domain/Models/DataTransferObjects/PosterDTO.cs
+++ API/OptimalAccessibility.Domain/Models/DataTransferObjects/PosterDTO.cs
@@ -1,13 +1,11 @@
-﻿using OptimalAccessibility.Domain.Models.Database;
-
-namespace OptimalAccessibility.Domain.Models.DataTransferObjects
+﻿namespace OptimalAccessibility.Domain.Models.DataTransferObjects
 {
     public class PosterDTO
     {
         public string PosterName { get; set; } = string.Empty;
         public string PosterImageTitle { get; set; } = string.Empty;
         public byte[]? PosterImageData { get; set; } = default;
-        public AccessibilityScore? AccessibilityScore { get; set; }
+        public AccessibilityScoreDTO? AccessibilityScore { get; set; }
     }
 }
 
