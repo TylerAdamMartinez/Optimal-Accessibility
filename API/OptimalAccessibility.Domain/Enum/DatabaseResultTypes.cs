d5cd13f953664851973305d185715c2e8859ab4c
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-03 23:07:45 -0500

    [Enum] feat: add DatabaseResultTypes enum

---
+++ API/OptimalAccessibility.Domain/Enum/DatabaseResultTypes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace OptimalAccessibility.Domain.Enum
+{
+    public enum DatabaseResultTypes
+    {
+        Successful = 0,
+        UpdateFailure,
+        UserNotFound,
+    }
+}
+
