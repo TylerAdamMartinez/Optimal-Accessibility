4bab84556b1b1751b5c70594f91472f5f87dfda7
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-07-11 16:56:48 -0500

    [NavBar] feat: Add upload new poster menu

--- Application/src/Components/NavBar.js
+++ Application/src/Components/NavBar.js
@@ -5,22 +5,37 @@ import HelpIcon from '@mui/icons-material/Help';
 import Popup from 'reactjs-popup';
 import OptimalAccessibilityLogo from './Optimal-Accessibility-Logo.png';
 import HelpPage from './HelpPage';
+import { useState } from 'react';
 
 function NavBar() {
   let Lorem = 
   `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu sem integer vitae justo. Non arcu risus quis varius quam. Posuere ac ut consequat semper. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Tincidunt praesent semper feugiat nibh sed. Leo vel orci porta non pulvinar neque laoreet. Mi sit amet mauris commodo quis imperdiet massa tincidunt. Ut venenatis tellus in metus. Pellentesque elit eget gravida cum sociis natoque penatibus. In vitae turpis massa sed elementum tempus egestas. Laoreet non curabitur gravida arcu ac tortor dignissim.
   Pellentesque dignissim enim sit amet venenatis. Tincidunt dui ut ornare lectus sit amet est placerat in. Rhoncus mattis rhoncus urna neque viverra justo. Tristique senectus et netus et malesuada fames. Consequat ac felis donec et odio. Dignissim suspendisse in est ante in. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Faucibus vitae aliquet nec ullamcorper sit. Magna fermentum iaculis eu non. Porttitor leo a diam sollicitudin tempor id.`;
 
-
-
   function MouseOverExitSpotHandler() {
     let close_popup_menu_element = document.querySelector('#ExitSpot p');
-    close_popup_menu_element.textContent = "Click here to close any popup window";
+    close_popup_menu_element.textContent = "close popup";
   }
 
   function MouseLeaveExitSpotHandler() {
     let close_popup_menu_element = document.querySelector('#ExitSpot p');
     close_popup_menu_element.textContent = "";
+  }
+
+  const [PosterName, SetPosterName] = useState('');
+  const [PosterFileData, SetPosterFileData] = useState(null);
+
+  function handleSubmit(event) {
+    alert(`PosterName="${PosterName}"\nPosterFileData="${PosterFileData}"`);
+    event.preventDefault();
+  }
+
+  function handleNameChange(event) {
+    SetPosterName(event.target.value)
+  }
+
+  function handleFileChange(event) {
+    SetPosterFileData(event.target.value)
   }
 
   return (
@@ -56,7 +71,12 @@ function NavBar() {
             <Popup trigger={<AddIcon fontSize="large"/>}>
               <div id="PopUpAddMenuDivSection">
                 <div id='PopUpAddMenuDiv'>
-                  Add new image page here..
+                  <h2>New Poster</h2>
+                  <form onSubmit={handleSubmit} id='PopUpAddMenuForm'>
+                    <input placeholder="Name" type="text" value={PosterName} onChange={handleNameChange} />
+                    <input placeholder="File" type="File" accept=".png, .jpg"  value={PosterFileData} onChange={handleFileChange}/>
+                    <input type="submit" value="Submit" className='PopUpAccountMenuDivbtn'/>
+                  </form>
                 </div>
               </div>
             </Popup>
