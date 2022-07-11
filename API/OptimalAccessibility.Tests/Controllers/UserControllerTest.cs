9932e8f62267eb38429a249a2af8453be2609cd7
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-11 01:28:18 -0500

    [UserController] build: Add needed test dependency

--- API/OptimalAccessibility.Tests/Controllers/UserControllerTest.cs
+++ API/OptimalAccessibility.Tests/Controllers/UserControllerTest.cs
@@ -12,6 +12,8 @@ using OptimalAccessibility.API;
 using Microsoft.AspNetCore.Http;
 using System.Security.Claims;
 using OptimalAccessibility.API.Utils;
+using Microsoft.Extensions.Configuration;
+using Swashbuckle.AspNetCore.Filters;
 
 namespace OptimalAccessibility.Tests.Controllers
 {
@@ -21,6 +23,7 @@ namespace OptimalAccessibility.Tests.Controllers
 
         public TestUserController(ITestOutputHelper output)
         {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
             var builder = new ServiceCollection()
                 .AddLogging(builder => builder.AddXUnit(output))
                 .AddDbContext<OptimalAccessibilityContext>(options =>
@@ -30,7 +33,8 @@ namespace OptimalAccessibility.Tests.Controllers
                 .AddTransient<IAuthRepo, AuthRepo>()
                 .AddTransient<AuthController>()
                 .AddTransient<IUserRepo, UsersRepo>()
-                .AddTransient<UserController>();
+                .AddTransient<UserController>()
+                .AddSingleton<IConfiguration>(configuration);
 
             _userController = builder.BuildServiceProvider().GetRequiredService<UserController>();
         }
@@ -39,12 +43,10 @@ namespace OptimalAccessibility.Tests.Controllers
         [Fact]
         public void Test()
         {
-            //var userId = new Guid("AAAAAAAA-BBBB-CCCC-DDDD-FFFFFFFFFFFF");
-            //var user = new ClaimsPrincipal(new OptimalAccessibilityIdentity(userId.ToString(), "Studnet"));
-            //_userController.ControllerContext = new ControllerContext();
-            //_userController.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
-
-            //_userController.GetPostersByUserId(userId);
+            var userId = new Guid("AAAAAAAA-BBBB-CCCC-DDDD-FFFFFFFFFFFF");
+            var user = new ClaimsPrincipal(new OptimalAccessibilityIdentity(userId.ToString(), "Studnet"));
+            _userController.ControllerContext = new ControllerContext();
+            _userController.ControllerContext.HttpContext = new DefaultHttpContext { User = user };
 
             Assert.True(true);
         }
