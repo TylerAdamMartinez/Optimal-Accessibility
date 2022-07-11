fbe0b19bb32cc6855e87dbc09d37978c890c2c8b
HankPeeples <hgpeeples48@gmail.com>
2022-07-11 13:42:25 -0500

    [Utils] color: Changed number of colors found

--- Application/src/Utils/Color.js
+++ Application/src/Utils/Color.js
@@ -2,39 +2,39 @@ import { prominent } from 'color.js';
 
 export async function getColors(image) {
   console.log('Getting colors');
-  await prominent(image.topLeft.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.topLeft.img, { amount: 2, format: 'hex' }).then((color) => {
     image.topLeft.color = color;
   });
 
-  await prominent(image.topMiddle.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.topMiddle.img, { amount: 2, format: 'hex' }).then((color) => {
     image.topMiddle.color = color;
   });
 
-  await prominent(image.topRight.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.topRight.img, { amount: 2, format: 'hex' }).then((color) => {
     image.topRight.color = color;
   });
 
-  await prominent(image.middleLeft.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.middleLeft.img, { amount: 2, format: 'hex' }).then((color) => {
     image.middleLeft.color = color;
   });
 
-  await prominent(image.middle.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.middle.img, { amount: 2, format: 'hex' }).then((color) => {
     image.middle.color = color;
   });
 
-  await prominent(image.middleRight.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.middleRight.img, { amount: 2, format: 'hex' }).then((color) => {
     image.middleRight.color = color;
   });
 
-  await prominent(image.bottomLeft.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.bottomLeft.img, { amount: 2, format: 'hex' }).then((color) => {
     image.bottomLeft.color = color;
   });
 
-  await prominent(image.bottomMiddle.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.bottomMiddle.img, { amount: 2, format: 'hex' }).then((color) => {
     image.bottomMiddle.color = color;
   });
 
-  await prominent(image.bottomRight.img, { amount: 1, format: 'hex' }).then((color) => {
+  await prominent(image.bottomRight.img, { amount: 2, format: 'hex' }).then((color) => {
     image.bottomRight.color = color;
   });
 
