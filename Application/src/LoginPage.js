b11a6c160ee88b6cb9bcbc2b178113e33fb19eb8
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-07-13 12:01:09 -0500

    [LoginPage] feat: Add post API call to api/Auth/Login

--- Application/src/LoginPage.js
+++ Application/src/LoginPage.js
@@ -4,12 +4,25 @@ import { Link } from 'react-router-dom';
 import Logo from './Components/Optimal-Accessibility-Logo.png';
 
 function Login() {
-    const [EUID, SetEUID] = useState('');
-    const [PASSWORD, SetPASSWORD] = useState('');
+    const [euid, SetEUID] = useState('');
+    const [password, SetPASSWORD] = useState('');
   
     function handleSubmit(event) {
-      alert(`PosterName="${EUID}"\nPosterFileData="${PASSWORD}"`);
-      event.preventDefault();
+        event.preventDefault();
+
+        const LoginBody = {euid, password};
+        console.log(JSON.stringify(LoginBody));
+        fetch('https://localhost:7267/api/Auth/Login', {
+            method : 'POST',
+            headers : {
+                "Content-Type" : "application/json",
+                "accept" : "application/json"
+            },
+            body : JSON.stringify(LoginBody)
+        })
+        .then((responce) => responce.json())
+        .then((responseJSON) => console.log(responseJSON))
+        .catch((err) => console.log(err));
     }
   
     function handleEUIDChange(event) {
@@ -28,11 +41,11 @@ function Login() {
 		    <div id='LoginPageSignInFormDiv'>
                 <h2>LOGIN</h2>
                 <form onSubmit={handleSubmit} id="SignInForm">
-                    <input placeholder="EUID" type="text" value={EUID} onChange={handleEUIDChange} />
-                    <input placeholder="PASSWORD" type="password"  value={PASSWORD} onChange={handlePASSWORDChange}/>
+                    <input placeholder="EUID" type="text" value={euid} onChange={handleEUIDChange} />
+                    <input placeholder="PASSWORD" type="password"  value={password} onChange={handlePASSWORDChange}/>
                     <div id='LoginBtnsSection'>
-                        <Link to="/register"><button className='PopUpAccountMenuDivbtn'>Sign Up</button></Link>
                         <input type="submit" value="Login" className='PopUpAccountMenuDivbtn'/>
+                        <Link to="/register"><button className='PopUpAccountMenuDivbtn'>Sign Up</button></Link>
                     </div>
                   </form>
             </div>
