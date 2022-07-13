62985006ac84e0049b8f101ce7dfd53d2cfba0a1
HankPeeples <hgpeeples48@gmail.com>
2022-07-13 12:32:39 -0500

    [Utils] text: Refactor into seperate function

--- Application/src/Utils/Text.js
+++ Application/src/Utils/Text.js
@@ -3,6 +3,37 @@ import { getColors } from './Color';
 
 export async function getText(image) {
   let grade = 0;
+  
+  [image, grade] = await findText(image, grade);
+
+  grade = grade / 9;
+  if (grade < 60 && grade > 0) {
+    grade += 40;
+  } else if (grade < 70 && grade > 60) {
+    grade += 30;
+  } else if (grade < 80 && grade > 70) {
+    grade += 20;
+  } else if (grade < 90 && grade > 80) {
+    grade += 20;
+  }
+
+  let cg;
+  let images;
+  await getColors(image).then(({ image, colorGrade }) => {
+    images = image;
+    cg = colorGrade;
+  });
+
+  let obj = {
+    images,
+    tg: grade,
+    cg,
+  };
+
+  return obj;
+}
+
+const findText = async (image, grade) => {
   // making sure Tesseract only runs when there is a valid image
   await Tesseract.recognize(image.topLeft.img, 'eng').then(({ data }) => {
     image.topLeft.textConfidence = data.confidence;
@@ -58,29 +89,5 @@ export async function getText(image) {
     image.bottomRight.text = data.text;
   });
 
-  grade = grade / 9;
-  if (grade < 60 && grade > 0) {
-    grade += 40;
-  } else if (grade < 70 && grade > 60) {
-    grade += 30;
-  } else if (grade < 80 && grade > 70) {
-    grade += 20;
-  } else if (grade < 90 && grade > 80) {
-    grade += 20;
-  }
-
-  let cg;
-  let images;
-  await getColors(image).then(({ image, colorGrade }) => {
-    images = image;
-    cg = colorGrade;
-  });
-
-  let obj = {
-    images,
-    tg: grade,
-    cg,
-  };
-
-  return obj;
-}
+  return [image, grade];
+};
