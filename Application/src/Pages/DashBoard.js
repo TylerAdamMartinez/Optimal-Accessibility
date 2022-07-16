61b7fdb11996a82baa90e35574535af5ba52cf09
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-07-16 13:34:20 -0500

    [src] refactor: mv AccessibilityBarGraphData into its own class

--- Application/src/Pages/DashBoard.js
+++ Application/src/Pages/DashBoard.js
@@ -3,9 +3,10 @@ import NavBar from './../Components/NavBar.js';
 //import TestImageComponent from './../Utils/TestImageComponent';
 import MyPostersSection from './../Components/MyPostersSection';
 import OverallAccessibilitySection from './../Components/OverallAccessibilitySection';
-import useParams from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Cookies from 'universal-cookie';
+import AccessibilityBarGraphData from '../Components/AccessibilityBarGraphData';
 
 function DashBoard() {
 	let params = useParams();
@@ -50,28 +51,7 @@ function DashBoard() {
 		.catch((err) => console.error(err));
 	}, [params]);
 
-	let OverallAccessibilityBarGraphData = {
-		labels: ['Text', 'Structure', 'Color'],
-		datasets: [
-			{
-				label: 'Rating Score',
-				backgroundColor: ['rgba(1, 127, 1, 1)', 'rgba(100, 6, 101, 1)', 'rgba(218, 54, 74, 1)'],
-				borderColor: 'rgba(51, 51, 51, 1)',
-				borderWidth: 1,
-				data: [ OverallAccessibilityScore.textRating, OverallAccessibilityScore.structureRating, OverallAccessibilityScore.colorRating],
-				options: {
-					responsive: true,
-					maintainAspectRatio: false,
-					scales: {
-						yAxes: {
-							min: 0,
-							max: 100,
-						},
-					},
-				},
-			},
-		],
-	};
+	let OverallAccessibilityBarGraphData = new AccessibilityBarGraphData(OverallAccessibilityScore);
 
 	return (
 		<>
@@ -81,7 +61,7 @@ function DashBoard() {
             NOTE: Console will show an error initially and then start showing text, this is not an issue with the text recognition but more with how I set up the component. */}
 				{/* <TestImageComponent /> */}
 				<MyPostersSection myPosters={Posters}/>
-				<OverallAccessibilitySection chartData={OverallAccessibilityBarGraphData} />
+				<OverallAccessibilitySection chartData={OverallAccessibilityBarGraphData.build} />
 			</div>
 		</>
 	);
