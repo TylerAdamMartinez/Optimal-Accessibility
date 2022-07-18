fbfe38724ef03a0ad9e5b03dc6130cb6fda3828c
Henry Peeples <66916746+hankpeeples@users.noreply.github.com>
2022-07-18 13:58:09 -0500

    [NavBar] Add poster rating function call (#33)
    * [NavBar]: Add initial poster rating function call
    
    * [NavBar]: getAccessibilityScore() returns valid score
    
    * [NavBar]: Change rating from float values to integer values

--- Application/src/Components/NavBar.js
+++ Application/src/Components/NavBar.js
@@ -8,77 +8,92 @@ import HelpPage from './HelpPage';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
+import { getImageGrid } from '../Utils/Structure';
 
 function NavBar() {
-  let Lorem = 
-  `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu sem integer vitae justo. Non arcu risus quis varius quam. Posuere ac ut consequat semper. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Tincidunt praesent semper feugiat nibh sed. Leo vel orci porta non pulvinar neque laoreet. Mi sit amet mauris commodo quis imperdiet massa tincidunt. Ut venenatis tellus in metus. Pellentesque elit eget gravida cum sociis natoque penatibus. In vitae turpis massa sed elementum tempus egestas. Laoreet non curabitur gravida arcu ac tortor dignissim.
+  let Lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu sem integer vitae justo. Non arcu risus quis varius quam. Posuere ac ut consequat semper. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Tincidunt praesent semper feugiat nibh sed. Leo vel orci porta non pulvinar neque laoreet. Mi sit amet mauris commodo quis imperdiet massa tincidunt. Ut venenatis tellus in metus. Pellentesque elit eget gravida cum sociis natoque penatibus. In vitae turpis massa sed elementum tempus egestas. Laoreet non curabitur gravida arcu ac tortor dignissim.
   Pellentesque dignissim enim sit amet venenatis. Tincidunt dui ut ornare lectus sit amet est placerat in. Rhoncus mattis rhoncus urna neque viverra justo. Tristique senectus et netus et malesuada fames. Consequat ac felis donec et odio. Dignissim suspendisse in est ante in. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Faucibus vitae aliquet nec ullamcorper sit. Magna fermentum iaculis eu non. Porttitor leo a diam sollicitudin tempor id.`;
 
   function MouseOverExitSpotHandler() {
     let close_popup_menu_element = document.querySelector('#ExitSpot p');
-    close_popup_menu_element.textContent = "close popup";
+    close_popup_menu_element.textContent = 'close popup';
   }
 
   function MouseLeaveExitSpotHandler() {
     let close_popup_menu_element = document.querySelector('#ExitSpot p');
-    close_popup_menu_element.textContent = "";
+    close_popup_menu_element.textContent = '';
   }
 
-  const [name, SetPosterName] = useState("");
-  const [FileData, SetPosterFileData] = useState("");
+  const [name, SetPosterName] = useState('');
+  const [FileData, SetPosterFileData] = useState('');
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(event) {
     event.preventDefault();
     let errorFlag = false;
 
     let title = Math.random().toString();
-    ///This should be replaced///
-    function HankFuncHere(data) {
-      return { textRating: 34, structureRating: 34, colorRating: 34};
-    }
-    ///This should be replaced///
-    let accessibilityScore = HankFuncHere(FileData);
-    ConvertImageToBase64(FileData)
-    .then((data) => {
-      const addPosterBody = {name, title, data, accessibilityScore};
-      let userId = localStorage.getItem('userId');
-      let cookies = new Cookies();
-      let Jwt = cookies.get('jwt');
 
-      fetch(`https://localhost:7267/api/User/AddPosterByUserId/${userId}`, {
-          method : 'POST',
-          headers : {
-              "Content-Type" : "application/json",
-              "accept" : "application/json",
-              "Authorization" : `bearer ${Jwt}`
-          },
-          body : JSON.stringify(addPosterBody)
-      })
-      .then((responce) => { 
-          if (!responce.ok) { 
-              errorFlag = true;
-          }
-          return responce.json();
-      })
-      .then((responseJSON) => {
-          if(errorFlag) {
-              throw new Error(`${responseJSON}`);
-          }
-          window.location.reload(false);
-      })
-      .catch((err) => {
-          alert(err);
-          console.error(err);
+    async function getAccessibilityScore(poster) {
+      setLoading(true);
+      poster = await ConvertImageToBase64(poster);
+      let posterGrades = await getImageGrid('data:image/png;base64,' + poster).then((score) => {
+        setLoading(false);
+        let posterGrade = {
+          textRating: Math.round(score.textGrade),
+          structureRating: Math.round(score.structureGrade),
+          colorRating: Math.round(score.colorGrade),
+        };
+
+        return posterGrade;
       });
-    })
-    .catch((Error) => {
-      alert(Error)
-      console.error(Error);
-    });
+
+      return posterGrades;
+    }
+
+    let accessibilityScore = await getAccessibilityScore(FileData);
+
+    ConvertImageToBase64(FileData)
+      .then((data) => {
+        const addPosterBody = { name, title, data, accessibilityScore };
+        let userId = localStorage.getItem('userId');
+        let cookies = new Cookies();
+        let Jwt = cookies.get('jwt');
+
+        fetch(`https://localhost:7267/api/User/AddPosterByUserId/${userId}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            accept: 'application/json',
+            Authorization: `bearer ${Jwt}`,
+          },
+          body: JSON.stringify(addPosterBody),
+        })
+          .then((responce) => {
+            if (!responce.ok) {
+              errorFlag = true;
+            }
+            return responce.json();
+          })
+          .then((responseJSON) => {
+            if (errorFlag) {
+              throw new Error(`${responseJSON}`);
+            }
+            window.location.reload(false);
+          })
+          .catch((err) => {
+            alert(err);
+            console.error(err);
+          });
+      })
+      .catch((Error) => {
+        alert(Error);
+        console.error(Error);
+      });
   }
 
   function handleNameChange(event) {
-    SetPosterName(event.target.value)
+    SetPosterName(event.target.value);
   }
 
   function handleFileChange(event) {
@@ -95,13 +110,12 @@ function NavBar() {
       };
 
       fileReader.onloadend = () => {
-        if(fileReader.result != null) {
+        if (fileReader.result != null) {
           resolve(fileReader.result.split(',')[1]);
-        }
-        else {
+        } else {
           reject(new Error(`fileReader.result is null`));
         }
-      }
+      };
 
       fileReader.onError = (err) => {
         console.error(err);
@@ -117,57 +131,70 @@ function NavBar() {
   }
 
   return (
-    <div className="NavBar">
+    <div className='NavBar'>
       <span></span>
-      <Link to="/">
-        <div id="LogoBox">
-          <img id="LogoImg" alt="Optimal Accessibility Logo" src={OptimalAccessibilityLogo} />
+      <Link to='/'>
+        <div id='LogoBox'>
+          <img id='LogoImg' alt='Optimal Accessibility Logo' src={OptimalAccessibilityLogo} />
           <h1>Optimal Accessibility</h1>
         </div>
       </Link>
-      <span id="ExitSpot" onMouseOver={MouseOverExitSpotHandler} onMouseLeave={MouseLeaveExitSpotHandler}>
+      <span
+        id='ExitSpot'
+        onMouseOver={MouseOverExitSpotHandler}
+        onMouseLeave={MouseLeaveExitSpotHandler}
+      >
         <p></p>
       </span>
-      <div id="NavItemsBox">
+      <div id='NavItemsBox'>
         <ul>
           <li>
-            <Popup trigger={<HelpIcon fontSize="large"/>}>
-              <div id="PopUpHelpMenuDivSection">
-                <ul id="PopUpHelpMenuDiv">
-                  <Popup trigger={<li id="PopUpHelpMenuDivTextField">Text</li>}>
-                    <HelpPage PageName="Text" PageContent={Lorem} Color={"#017F01"}/>
+            <Popup trigger={<HelpIcon fontSize='large' />}>
+              <div id='PopUpHelpMenuDivSection'>
+                <ul id='PopUpHelpMenuDiv'>
+                  <Popup trigger={<li id='PopUpHelpMenuDivTextField'>Text</li>}>
+                    <HelpPage PageName='Text' PageContent={Lorem} Color={'#017F01'} />
                   </Popup>
-                  <Popup trigger={<li id="PopUpHelpMenuDivStructureField">Structure</li>}>
-                    <HelpPage PageName="Structure" PageContent={Lorem} Color={"#640665"}/>
+                  <Popup trigger={<li id='PopUpHelpMenuDivStructureField'>Structure</li>}>
+                    <HelpPage PageName='Structure' PageContent={Lorem} Color={'#640665'} />
                   </Popup>
-                  <Popup trigger={<li id="PopUpHelpMenuDivColorField">Color</li>}>
-                  <HelpPage PageName="Color" PageContent={Lorem} Color={"#DA364A"}/>
+                  <Popup trigger={<li id='PopUpHelpMenuDivColorField'>Color</li>}>
+                    <HelpPage PageName='Color' PageContent={Lorem} Color={'#DA364A'} />
                   </Popup>
                 </ul>
               </div>
             </Popup>
           </li>
           <li>
-            <Popup trigger={<AddIcon fontSize="large"/>}>
-              <div id="PopUpAddMenuDivSection">
+            <Popup trigger={<AddIcon fontSize='large' />}>
+              <div id='PopUpAddMenuDivSection'>
                 <div id='PopUpAddMenuDiv'>
                   <h2>New Poster</h2>
                   <form onSubmit={handleSubmit} id='PopUpAddMenuForm'>
-                    <input placeholder="Name" type="text" value={name} onChange={handleNameChange} />
-                    <input type="File" accept=".png, .jpg" onChange={handleFileChange}/>
-                    <input type="submit" value="Submit" className='PopUpAccountMenuDivbtn'/>
+                    {/* Either remove before finished or make it look nice, would be nice to show that we are loading */}
+                    {loading ? <h3>Loading...</h3> : null}
+                    <input
+                      placeholder='Name'
+                      type='text'
+                      value={name}
+                      onChange={handleNameChange}
+                    />
+                    <input type='File' accept='.png, .jpg' onChange={handleFileChange} />
+                    <input type='submit' value='Submit' className='PopUpAccountMenuDivbtn' />
                   </form>
                 </div>
               </div>
             </Popup>
           </li>
           <li>
-            <Popup trigger={<AccountCircleIcon fontSize="large"/>}>
-              <div id="PopUpAccountpMenuDivSection">
-                  <ul id="PopUpAccountMenuDiv">
-                  <Link to="/" onClick={handleLogout}><li className='PopUpAccountMenuDivbtn'>Logout</li></Link>
-                  </ul>
-                </div>
+            <Popup trigger={<AccountCircleIcon fontSize='large' />}>
+              <div id='PopUpAccountpMenuDivSection'>
+                <ul id='PopUpAccountMenuDiv'>
+                  <Link to='/' onClick={handleLogout}>
+                    <li className='PopUpAccountMenuDivbtn'>Logout</li>
+                  </Link>
+                </ul>
+              </div>
             </Popup>
           </li>
         </ul>
@@ -178,5 +205,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
-
