9f04d8819b864d1c84db3bcd10e3e82228025394
Henry Peeples <66916746+hankpeeples@users.noreply.github.com>
2022-07-19 16:18:38 -0500

    [Pages] Update web application user interface (#34)
    * [LoginPage]: Add some CSS adjustments
    
    * [RegistrationPage]: Adjust simple CSS
    
    * [404Page]: Very simple button CSS fix
    
    * [Posters]: Adjust image sizing and hover effect
    
    * [NavBar]: Add border radius to help feilds
    
    * [Posters]: Adjust font size and padding
    
    * [HelpPage/NavBar]: Add shadows
    
    * [NavBar/AddNew]: Adjust border radius, spacing, and shadows
    
    * [BarGraph]: Always show 0 to 100
    
    * [Buttons]: Add box-shadow hover transitions
    
    * [MyPoster] popup: Add image sizing constraints and adjust layout
    
    * [MyPoster] popup: Add delete and edit icons
    
    * [NavBar] feat: add loading states to posting uploading
    
    Co-authored-by: Tyler Adam Martinez <TylerAdamMartinez@outlook.com>

--- Application/src/Components/AccessibilityBarGraphData.js
+++ Application/src/Components/AccessibilityBarGraphData.js
@@ -1,32 +1,30 @@
 class AccessibilityBarGraphData {
-    constructor(AccessibilityScore) {
-        this.AccessibilityScore = AccessibilityScore;
-    }
+  constructor(AccessibilityScore) {
+    this.AccessibilityScore = AccessibilityScore;
+  }
 
-    get build() {
-        return {
-            labels: ['Text', 'Structure', 'Color'],
-            datasets: [
-                {
-                    label: 'Rating Score',
-                    backgroundColor: ['rgba(1, 127, 1, 1)', 'rgba(100, 6, 101, 1)', 'rgba(218, 54, 74, 1)'],
-                    borderColor: 'rgba(51, 51, 51, 1)',
-                    borderWidth: 1,
-                    data: [ this.AccessibilityScore.textRating, this.AccessibilityScore.structureRating, this.AccessibilityScore.colorRating],
-                    options: {
-                        responsive: true,
-                        maintainAspectRatio: false,
-                        scales: {
-                            yAxes: {
-                                min: 0,
-                                max: 100,
-                            },
-                        },
-                    },
-                },
-            ],
-        };
-    }
+  get build() {
+    return {
+      labels: ['Text', 'Structure', 'Color'],
+      datasets: [
+        {
+          label: 'Rating Score',
+          backgroundColor: ['rgba(1, 127, 1, 1)', 'rgba(100, 6, 101, 1)', 'rgba(218, 54, 74, 1)'],
+          borderColor: 'rgba(51, 51, 51, 1)',
+          borderWidth: 1,
+          data: [
+            this.AccessibilityScore.textRating,
+            this.AccessibilityScore.structureRating,
+            this.AccessibilityScore.colorRating,
+          ],
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+          },
+        },
+      ],
+    };
+  }
 }
 
 export default AccessibilityBarGraphData;
