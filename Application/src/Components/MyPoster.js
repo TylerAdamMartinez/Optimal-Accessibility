dc05d659d2448cd35bb1a3ffa63b7c0330d4a089
HenryPeeples <hgpeeples48@gmail.com>
2022-07-19 12:08:32 -0500

    [MyPoster] popup: Add image sizing constraints and adjust layout

--- Application/src/Components/MyPoster.js
+++ Application/src/Components/MyPoster.js
@@ -6,47 +6,62 @@ import { useState, useRef } from 'react';
 import AccessibilityBarGraphData from './AccessibilityBarGraphData';
 
 function MyPoster(props) {
-    const imgRef = useRef();
-    function onImageError() {
-      imgRef.current.src = DefaultImage; 
-    }
+  const imgRef = useRef();
+  function onImageError() {
+    imgRef.current.src = DefaultImage;
+  }
 
-    let [isOpen, setIsOpen] = useState(false);
+  let [isOpen, setIsOpen] = useState(false);
 
-    function handleOpen() {
-      setIsOpen(false);
-    }
+  function handleOpen() {
+    setIsOpen(false);
+  }
 
-    let BarGraphData = new AccessibilityBarGraphData(props.AccessibilityRating);
+  let BarGraphData = new AccessibilityBarGraphData(props.AccessibilityRating);
 
-    return (
-        <Popup 
-        trigger={
-          <div id="MyPoster">
-              <div id="PosterImage">
-                  <img src={`data:image/png;base64,${props.Data}`} ref={imgRef} onError={onImageError} alt={`Poster number ${props.Id}`}/>
-              </div>
-              <div id="PosterNameSection">
-                  <h3>{props.PosterName}</h3>
-              </div>
-          </div>}
-        open={isOpen}
-        onOpen={handleOpen}
-        >
-            <div id="PosterPopUpMenuDiv">
-                <div id="PosterPopUpMenuPosterNameDiv">
-                  <h3>{props.PosterName}</h3>
-                </div>
-                <div id="PosterPopUpMenuImgDiv">
-                  <img ref={imgRef} onError={onImageError} src={`data:image/png;base64,${props.Data}`} alt={`Poster number ${props.Id}`}/>
-                </div>
-                <h3>Overall Accessibility Score</h3>
-                <div id="PosterPopUpMenuBarGraphDiv">
-                  <BarGraph chartData={BarGraphData.build}/>
-                </div>
-            </div>  
-        </Popup>
-    );
+  return (
+    <Popup
+      trigger={
+        <div id='MyPoster'>
+          <div id='PosterImage'>
+            <img
+              src={`data:image/png;base64,${props.Data}`}
+              ref={imgRef}
+              onError={onImageError}
+              alt={`Poster number ${props.Id}`}
+            />
+          </div>
+          <div id='PosterNameSection'>
+            <h3>{props.PosterName}</h3>
+          </div>
+        </div>
+      }
+      open={isOpen}
+      onOpen={handleOpen}
+    >
+      <div id='PosterPopUpMenuDiv'>
+        <div className='PosterImgAndNameContainer'>
+          <div id='PosterPopUpMenuPosterNameDiv'>
+            <h3>{props.PosterName}</h3>
+          </div>
+          <div id='PosterPopUpMenuImgDiv'>
+            <img
+              ref={imgRef}
+              onError={onImageError}
+              src={`data:image/png;base64,${props.Data}`}
+              alt={`Poster number ${props.Id}`}
+            />
+          </div>
+        </div>
+        <div className='AccessibilityBarGraphScoreContainer'>
+          <h3>Accessibility Score</h3>
+          <div id='PosterPopUpMenuBarGraphDiv'>
+            <BarGraph chartData={BarGraphData.build} />
+          </div>
+        </div>
+      </div>
+    </Popup>
+  );
 }
 
 export default MyPoster;
