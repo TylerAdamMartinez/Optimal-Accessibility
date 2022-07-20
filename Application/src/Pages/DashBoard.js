c858ce40b5fe6a523fca8488cb54ceda5940c488
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2022-07-20 11:33:50 -0500

    [DashBoard] fix: rm user Guid from url

--- Application/src/Pages/DashBoard.js
+++ Application/src/Pages/DashBoard.js
@@ -1,22 +1,19 @@
 import './../App.css';
 import NavBar from './../Components/NavBar.js';
-//import TestImageComponent from './../Utils/TestImageComponent';
 import MyPostersSection from './../Components/MyPostersSection';
 import OverallAccessibilitySection from './../Components/OverallAccessibilitySection';
-import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Cookies from 'universal-cookie';
 import AccessibilityBarGraphData from '../Components/AccessibilityBarGraphData';
 
 function DashBoard() {
-	let params = useParams();
-
 	let [OverallAccessibilityScore, SetOverallAccessibilityScore] = useState({textRating: 5, structureRating: 5, colorRating: 5});
 	useEffect(() => {
 		let cookies = new Cookies();
 		let Jwt = cookies.get('jwt');
+		let userId = localStorage.getItem('userId');
 
-		fetch(`https://localhost:7267/api/User/GetOverallAccessibilityScoreByUserId/${params.userId}`, {
+		fetch(`https://localhost:7267/api/User/GetOverallAccessibilityScoreByUserId/${userId}`, {
 			method : 'GET',
 			headers : {
 				"Content-Type" : "application/json",
@@ -29,14 +26,15 @@ function DashBoard() {
 			SetOverallAccessibilityScore(responseJSON);
 		})
 		.catch((err) => console.error(err));
-	}, [params]);
+	}, []);
 
 	const [Posters, SetPosters] = useState([{name: "string", data: "", accessibilityScore: {textRating: 55, structureRating: 55, colorRating: 55}}]);
 	useEffect(() => {
 		let cookies = new Cookies();
 		let Jwt = cookies.get('jwt');
+		let userId = localStorage.getItem('userId');
 
-		fetch(`https://localhost:7267/api/User/GetPostersByUserId/${params.userId}`, {
+		fetch(`https://localhost:7267/api/User/GetPostersByUserId/${userId}`, {
 			method : 'GET',
 			headers : {
 				"Content-Type" : "application/json",
@@ -49,7 +47,7 @@ function DashBoard() {
 			SetPosters(responseJSON);
 		})
 		.catch((err) => console.error(err));
-	}, [params]);
+	}, []);
 
 	let OverallAccessibilityBarGraphData = new AccessibilityBarGraphData(OverallAccessibilityScore);
 
@@ -57,9 +55,6 @@ function DashBoard() {
 		<>
 			<NavBar />
 			<div className='App'>
-				{/* Uncomment component below to see it working. 
-            NOTE: Console will show an error initially and then start showing text, this is not an issue with the text recognition but more with how I set up the component. */}
-				{/* <TestImageComponent /> */}
 				<MyPostersSection myPosters={Posters}/>
 				<OverallAccessibilitySection chartData={OverallAccessibilityBarGraphData.build} />
 			</div>
