5a26e70439d0f7b3891dfd36245e3ce1ca8be8cf
HenryPeeples <hgpeeples48@gmail.com>
2022-07-20 13:12:27 -0500

    [HelpPage]: Add markdown rendering for text

--- Application/src/Components/HelpPage.js
+++ Application/src/Components/HelpPage.js
@@ -1,19 +1,21 @@
-import "./HelpPage.css";
-
-
+import './HelpPage.css';
+import ReactMarkdown from 'react-markdown';
+import rehypeRaw from 'rehype-raw';
+import remarkGfm from 'remark-gfm';
 
 function HelpPage(props) {
-
-
-    return (
-        <div id="OuterHelpPageDIV">
-            <div id="InnerHelpPageDIV">
-                <h3 style={{backgroundColor: props.Color}}>{props.PageName}</h3>
-                <p>{props.PageContent}</p>
-            </div>
+  return (
+    <div id='OuterHelpPageDIV'>
+      <div id='InnerHelpPageDIV'>
+        <h3 style={{ backgroundColor: props.Color }}>{props.PageName}</h3>
+        <div>
+          <ReactMarkdown rehypePlugins={rehypeRaw} remarkPlugins={remarkGfm}>
+            {props.PageContent}
+          </ReactMarkdown>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
-
 
 export default HelpPage;
