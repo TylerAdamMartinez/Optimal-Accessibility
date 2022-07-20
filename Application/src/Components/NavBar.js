6a13cd49e95beb8c47e49e2b6833f3a4e40c4230
HenryPeeples <hgpeeples48@gmail.com>
2022-07-20 13:12:48 -0500

    [NavBar]: Add help info text for each category

--- Application/src/Components/NavBar.js
+++ Application/src/Components/NavBar.js
@@ -11,8 +11,31 @@ import Cookies from 'universal-cookie';
 import { getImageGrid } from '../Utils/Structure';
 
 function NavBar() {
-  let Lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu sem integer vitae justo. Non arcu risus quis varius quam. Posuere ac ut consequat semper. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Tincidunt praesent semper feugiat nibh sed. Leo vel orci porta non pulvinar neque laoreet. Mi sit amet mauris commodo quis imperdiet massa tincidunt. Ut venenatis tellus in metus. Pellentesque elit eget gravida cum sociis natoque penatibus. In vitae turpis massa sed elementum tempus egestas. Laoreet non curabitur gravida arcu ac tortor dignissim.
-  Pellentesque dignissim enim sit amet venenatis. Tincidunt dui ut ornare lectus sit amet est placerat in. Rhoncus mattis rhoncus urna neque viverra justo. Tristique senectus et netus et malesuada fames. Consequat ac felis donec et odio. Dignissim suspendisse in est ante in. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci. Faucibus vitae aliquet nec ullamcorper sit. Magna fermentum iaculis eu non. Porttitor leo a diam sollicitudin tempor id.`;
+  let textHelpInfo = `The text rating is mainly based on the readability of the text. If the text cannot be easily read by the computer, then it probably can't be easily read by a person. The size of the text, as well as the color contrast with its surroundings, are the largest factors.
+
+If the text rating for your poster is low, the following list could help you find some issues:
+- Text color vs. background color
+- Text size (not too small or too big)
+- Text orientation(vertical text can't be read easily)
+- Spelling
+`;
+
+  let structureHelpInfo = `The structure rating uses both text and a split-up, 3x1 grid of the original image. Text size is compared with its parent section's location, whether it be top, middle, or bottom to find out if that specific text could be considered a heading or simply informative text. The final structure rating also uses the ratings from text and color to average itself out.
+
+If the structure rating for your poster is low, the following list could help you find some issues:
+
+- A low text rating can affect the structure
+- A low color rating can affect the structure
+- Large font in an area that shouldn't be used as a heading
+`;
+
+  let colorHelpInfo = `The color rating uses a 3x3 grid of the original image to find and evaluate color contrast. The color contrast is based on various colors found in the background and text of each grid section. The application uses a color contrast testing tool to figure out whether the colors are suitable to be used in close proximity to each other.
+
+If the color rating for your poster is low, the following list could help you find some issues:
+
+- Text color may not have enough contrast with its background
+- Different colors that are in close proximity may have low contrast
+`;
 
   function MouseOverExitSpotHandler() {
     let close_popup_menu_element = document.querySelector('#ExitSpot p');
@@ -26,7 +49,7 @@ function NavBar() {
 
   const [name, SetPosterName] = useState('');
   const [FileData, SetPosterFileData] = useState('');
-  const [loadingState, setLoadingState] = useState("submit");
+  const [loadingState, setLoadingState] = useState('submit');
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -35,9 +58,9 @@ function NavBar() {
     let title = Math.random().toString();
 
     async function getAccessibilityScore(poster) {
-      setLoadingState("Uploading image...");
+      setLoadingState('Uploading image...');
       poster = await ConvertImageToBase64(poster);
-      setLoadingState("Calculating accessibility score...")
+      setLoadingState('Calculating accessibility score...');
       let posterGrades = await getImageGrid('data:image/png;base64,' + poster).then((score) => {
         let posterGrade = {
           textRating: Math.round(score.textGrade),
@@ -47,12 +70,12 @@ function NavBar() {
 
         return posterGrade;
       });
-      
+
       return posterGrades;
     }
 
     let accessibilityScore = await getAccessibilityScore(FileData);
-    setLoadingState("Sending Poster...");
+    setLoadingState('Sending Poster...');
     ConvertImageToBase64(FileData)
       .then((data) => {
         const addPosterBody = { name, title, data, accessibilityScore };
@@ -73,8 +96,8 @@ function NavBar() {
             if (!responce.ok) {
               errorFlag = true;
             }
-            
-            setLoadingState("Sent");
+
+            setLoadingState('Sent');
             return responce.json();
           })
           .then((responseJSON) => {
@@ -155,20 +178,33 @@ function NavBar() {
               <div id='PopUpHelpMenuDivSection'>
                 <ul id='PopUpHelpMenuDiv'>
                   <Popup trigger={<li id='PopUpHelpMenuDivTextField'>Text</li>}>
-                    <HelpPage PageName='Text' PageContent={Lorem} Color={'#017F01'} />
+                    <HelpPage PageName='Text' PageContent={textHelpInfo} Color={'#017F01'} />
                   </Popup>
                   <Popup trigger={<li id='PopUpHelpMenuDivStructureField'>Structure</li>}>
-                    <HelpPage PageName='Structure' PageContent={Lorem} Color={'#640665'} />
+                    <HelpPage
+                      PageName='Structure'
+                      PageContent={structureHelpInfo}
+                      Color={'#640665'}
+                    />
                   </Popup>
                   <Popup trigger={<li id='PopUpHelpMenuDivColorField'>Color</li>}>
-                    <HelpPage PageName='Color' PageContent={Lorem} Color={'#DA364A'} />
+                    <HelpPage PageName='Color' PageContent={colorHelpInfo} Color={'#DA364A'} />
                   </Popup>
                 </ul>
               </div>
             </Popup>
           </li>
           <li>
-            <Popup trigger={<AddIcon onClick={() => {setLoadingState("Submit")}} fontSize='large' />}>
+            <Popup
+              trigger={
+                <AddIcon
+                  onClick={() => {
+                    setLoadingState('Submit');
+                  }}
+                  fontSize='large'
+                />
+              }
+            >
               <div id='PopUpAddMenuDivSection'>
                 <div id='PopUpAddMenuDiv'>
                   <h2>New Poster</h2>
@@ -180,11 +216,7 @@ function NavBar() {
                       onChange={handleNameChange}
                     />
                     <input type='File' accept='.png, .jpg' onChange={handleFileChange} />
-                    <input
-                      type='submit'
-                      value={loadingState}
-                      className='PopUpAccountMenuDivbtn'
-                    />
+                    <input type='submit' value={loadingState} className='PopUpAccountMenuDivbtn' />
                   </form>
                 </div>
               </div>
