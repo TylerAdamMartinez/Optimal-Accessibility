bf8093123613c7313ecec7d59076a15d24dab2aa
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2022-07-21 09:45:01 -0500

    [ReportTemplates] feat: add report template

---
+++ API/OptimalAccessibility.Domain/Models/DataTransferObjects/PosterReportDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OptimalAccessibility.Domain.Models.DataTransferObjects
+{
+    public class PosterReportDetails
+    {
+        public string Name { get; set; } = string.Empty;
+        public AccessibilityScoreDTO? AccessibilityScore { get; set; }
+    }
+}
+
