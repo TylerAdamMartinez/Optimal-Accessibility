c5883821add57c065d046ec8a600ebf067335284
HenryPeeples <hgpeeples48@gmail.com>
2022-07-21 21:35:36 -0500

    [Utils] color: Fix bug with contrast ratio calc

--- Application/src/Utils/Color.js
+++ Application/src/Utils/Color.js
@@ -2,76 +2,51 @@ import { prominent } from 'color.js';
 import { colorContrastRatioCalculator } from '@mdhnpm/color-contrast-ratio-calculator';
 
 export async function getColors(image) {
+  let cgArr = [];
   await prominent(image.topLeft.img, { amount: 3, format: 'hex' }).then((color) => {
     image.topLeft.color = color;
+    cgArr[0] = getScore(image.topLeft.color);
   });
 
   await prominent(image.topMiddle.img, { amount: 3, format: 'hex' }).then((color) => {
     image.topMiddle.color = color;
+    cgArr[1] = getScore(image.topMiddle.color);
   });
 
   await prominent(image.topRight.img, { amount: 3, format: 'hex' }).then((color) => {
     image.topRight.color = color;
+    cgArr[2] = getScore(image.topRight.color);
   });
 
   await prominent(image.middleLeft.img, { amount: 3, format: 'hex' }).then((color) => {
     image.middleLeft.color = color;
+    cgArr[3] = getScore(image.middleLeft.color);
   });
 
   await prominent(image.middle.img, { amount: 3, format: 'hex' }).then((color) => {
     image.middle.color = color;
+    cgArr[4] = getScore(image.middle.color);
   });
 
   await prominent(image.middleRight.img, { amount: 3, format: 'hex' }).then((color) => {
     image.middleRight.color = color;
+    cgArr[5] = getScore(image.middleRight.color);
   });
 
   await prominent(image.bottomLeft.img, { amount: 3, format: 'hex' }).then((color) => {
     image.bottomLeft.color = color;
+    cgArr[6] = getScore(image.bottomLeft.color);
   });
 
   await prominent(image.bottomMiddle.img, { amount: 3, format: 'hex' }).then((color) => {
     image.bottomMiddle.color = color;
+    cgArr[7] = getScore(image.bottomMiddle.color);
   });
 
   await prominent(image.bottomRight.img, { amount: 3, format: 'hex' }).then((color) => {
     image.bottomRight.color = color;
+    cgArr[8] = getScore(image.bottomRight.color);
   });
-
-  let cgArr = [];
-  for (let i = 0; i < 9; i++) {
-    switch (i) {
-      case 0:
-        cgArr[i] = getScore(image.topLeft.color);
-        break;
-      case 1:
-        cgArr[i] = getScore(image.topMiddle.color);
-        break;
-      case 2:
-        cgArr[i] = getScore(image.topRight.color);
-        break;
-      case 3:
-        cgArr[i] = getScore(image.middleLeft.color);
-        break;
-      case 4:
-        cgArr[i] = getScore(image.middle.color);
-        break;
-      case 5:
-        cgArr[i] = getScore(image.middleRight.color);
-        break;
-      case 6:
-        cgArr[i] = getScore(image.bottomLeft.color);
-        break;
-      case 7:
-        cgArr[i] = getScore(image.bottomMiddle.color);
-        break;
-      case 8:
-        cgArr[i] = getScore(image.bottomRight.color);
-        break;
-      default:
-        break;
-    }
-  }
 
   let colorGrade = 0;
   cgArr.forEach((grade) => {
@@ -85,7 +60,7 @@ export async function getColors(image) {
   } else if (colorGrade >= 30 && colorGrade < 50) {
     colorGrade += 45;
   } else {
-    colorGrade += 55;
+    colorGrade += 45;
   }
 
   let obj = {
@@ -99,9 +74,14 @@ export async function getColors(image) {
 const getScore = (color) => {
   let scores = [];
   let high = 0;
-  scores[0] = colorContrastRatioCalculator(color[0], color[1]);
-  scores[1] = colorContrastRatioCalculator(color[0], color[2]);
-  scores[2] = colorContrastRatioCalculator(color[1], color[2]);
+  if (color.length === 3) {
+    scores[0] = colorContrastRatioCalculator(color[0], color[1]);
+    scores[1] = colorContrastRatioCalculator(color[0], color[2]);
+    scores[2] = colorContrastRatioCalculator(color[1], color[2]);
+  } else {
+    // If 'color' received by getScore() is only one value, no need to find contrast ratio for that square
+    return 15;
+  }
 
   scores.forEach((score) => {
     if (score > high) high = score;
