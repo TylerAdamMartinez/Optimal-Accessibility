c8da9890869c3338d2d1df51fcd57a8e276d9f25
HenryPeeples <hgpeeples48@gmail.com>
2022-08-03 12:15:30 -0500

    Updates

--- API/OptimalAccessibility.Domain/Models/Auth/LoginUserResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OptimalAccessibility.Domain.Models.Auth
-{
-    public class LoginUserResponse
-    {
-        public string Jwt { get; set; } = string.Empty;
-        public Guid userId { get; set; } 
-    }
-}
-
